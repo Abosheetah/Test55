4e8f3d10802f2493ad646475f389c0abe44e5087
abosheetah <abosheetah@gmail.com>
2020-09-29 22:58:48 +0200

    first commit

---
+++ MamSH.AlArishUniversityTest.Clint/Server/_DbContext/DbContextAlArish.cs
@@ -0,0 +1,26 @@
+﻿using MamSH.AlArishUniversityTest.Clint.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MamSH.AlArishUniversityTest.Clint.Server._DbContext
+{
+    public class DbContextAlArish : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+        }
+    }
+}
---
+++ MamSH.AlArishUniversityTest.Clint/Shared/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MamSH.AlArishUniversityTest.Clint.Shared
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Student Name is Requierd")]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
